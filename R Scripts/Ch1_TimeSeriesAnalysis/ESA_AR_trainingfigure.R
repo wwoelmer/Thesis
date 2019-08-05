8f90b01fc6732c9f07f719fba8a0c6cf3cb14458
wwoel <wwoelmer@vt.edu>
2019-08-05 08:36:09 -0400

    new files

---
+++ R Scripts/Ch1_TimeSeriesAnalysis/ESA_AR_trainingfigure.R
@@ -0,0 +1,51 @@
+# script to create the 2013-2016 times series for model training for the AR chl model for FCR
+
+library(MuMIn)
+library(knitr)
+library(rsq)
+library(tidyverse)
+library(Metrics)
+
+
+data <- read.csv("./Data/ARIMA_data/data_arima_WW.csv")
+data$Date <- as.Date(data$Date)
+# get rid of NAs at beginning
+data <- data[data$Date>"2013-05-09",]
+# because rmse retains native units, create a chl column that is not square rooted
+data <- data %>% mutate(Chla_ugL = (Chla_sqrt)^2)
+
+# now the model equation!!!!!!
+data <- data %>% mutate(pred = 1.65 + 0.45*Chla_ARlag1_sqrt - 3.05*mean_flow - 0.0025*ShortWave_mean)
+
+mod1_1316 <- glm(Chla_sqrt~Chla_ARlag1_sqrt + mean_flow +ShortWave_mean, 
+                 data = data, family = gaussian, na.action = 'na.fail')
+# make predictions
+pred1_1316 <- predict(mod1_1316, newdata = data)
+
+# plot the predictions for the entire dataset
+plot(data$Date, data$Chla_ugL, type = 'l')
+points(data$Date, (pred1_1316)^2, col = 'red', type = 'l')
+
+# subset each of the years so the plot is still 2013-2016 but each line stops in october
+# and doesn't connect to the may of the following year??
+
+a <- data[data$Date < "2013-12-01",]
+a$Date <- as.Date(a$Date)
+
+b <- data[data$Date < "2014-12-01" & data$Date > "2013-12-01",]
+b$Date <- as.Date(b$Date)
+
+c <- data[data$Date < "2015-12-01" & data$Date > "2014-12-01",]
+c$Date <- as.Date(c$Date)
+
+d <- data[data$Date < "2016-12-01" & data$Date > "2015-12-01",]
+d$Date <- as.Date(d$Date)
+
+plot(data$Date, (data$Chla_sqrt)^2, cex.lab = 1.5, type = 'n', xlab = "Date", ylab = "Chlorophyll a (ug/L)", xlim = c(as.Date("2013-01-01"), as.Date("2016-12-31")))
+points(a$Date, (a$Chla_sqrt)^2, lwd = 2, type = 'p', pch = 16)
+points(b$Date, (b$Chla_sqrt)^2, lwd = 2, type = 'p', pch = 16)
+points(c$Date, (c$Chla_sqrt)^2, lwd = 2, type = 'p', pch = 16)
+points(d$Date, (d$Chla_sqrt)^2, lwd = 2, type = 'p', pch = 16)
+points(data$Date, (pred1_1316)^2, col = '#fc8d59', type = 'l', lwd = 2)
+legend('topleft', c('Observed', 'AR Model'),cex = 1.5,  lty = c(1,1), col = c('black', '#fc8d59'), bty = 'n')
+
