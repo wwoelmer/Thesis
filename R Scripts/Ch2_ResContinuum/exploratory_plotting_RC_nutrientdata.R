1c9301ffb91a4223162bcd2d59225f6f9eb33414
wwoel <wwoelmer@vt.edu>
2019-12-09 15:41:09 -0500

    new script starting to look at RCC nutrient data

---
+++ R Scripts/Ch2_ResContinuum/exploratory_plotting_RC_nutrientdata.R
@@ -0,0 +1,20 @@
+# preliminary exploration of RCC nutrient data
+library(tidyverse)
+
+nuts <- read.csv('./Data/analytical chemistry/Lachat 2019/chemistry_working_half.csv')
+nuts <- nuts[nuts$Depth_m==0.1,]
+nuts$Date <- as.Date(nuts$Date)
+
+ggplot(data = nuts, aes(x = Date, y = NH4_ppb )) +facet_wrap(~Site) +geom_line()
+
+ggplot(data = nuts, aes(x = Date, y = NH4_ppb, col = Site ))  +geom_line() +facet_wrap(~Reservoir)
+ggplot(data = nuts[nuts$Date==as.Date('2019-06-27'),], aes(x = Site, y = NH4_ppb) ) + geom_line()
+ggplot(data = nuts[nuts$Date==as.Date('2019-07-18'),], aes(x = Site, y = NH4_ppb) ) + geom_line()
+ggplot(data = nuts[nuts$Date==as.Date('2019-05-30'),], aes(x = Site, y = NH4_ppb) ) + geom_line()
+
+ggplot(data = nuts[nuts$Site=='F200',], aes(x = Date, y = NH4_ppb)) + geom_line() 
+
+may <- nuts[nuts$Date=='2019-05-30',]
+plot(may$Site, may$NH4_ppb, type = 'p')
+points(may$Site, may$NO3NO2_ppb, col = 'orange')
+points(may$Site, may$PO4_ppb, col = 'purple')
